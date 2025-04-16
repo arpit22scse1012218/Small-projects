69070e0cf57883601e935ebb0c0d27257e4ddd92
Arpit Pandey <arpitpandey084@gmail.com>
2025-04-16 13:44:08 +0530

    Add files via upload

---
+++ small projects/faulty calculator/faulty.js
@@ -0,0 +1,12 @@
+let a=Math.random(1,100);
+let s=prompt("Enter the value of a");
+let t=prompt("Enter the value of b");
+let k=prompt("enter the function")
+if(a<10)
+{
+    console.log(a k t);
+
+}
+else{
+    if(k==)
+}
